727df8d198dfdb6830f36f62e56d7f9e6c2a9d3b
terry chay <tychay@php.net>
2017-04-24 10:14:46 -0700

    simple PHP script to scrape from the API
    Signed-off-by: terry chay <tychay@php.net>

---
+++ scraper.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Goes to a URL endpoint and returns the HTTP body as a JSON object
+ * @param  string $url The URL to hit
+ * @return array|false
+ */
+function getUrlJson($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$data = curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	return ($httpcode>=200 && $httpcode<300) ? json_decode($data) : false;
+}
+
+print_r(getUrlJson('http://raise-me-take-home.raise.me'));
+
+?>
