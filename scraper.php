0f4d237fdf303f0db5fa5e679eff4015d09b7da4
terry chay <tychay@php.net>
2017-04-24 11:11:16 -0700

    grade computation and first PUT call in elasticsearch
    Signed-off-by: terry chay <tychay@php.net>

--- scraper.php
+++ scraper.php
@@ -17,6 +17,28 @@ function getUrlJson($url){
 	curl_close($ch);
 	return ($httpcode>=200 && $httpcode<300) ? json_decode($data) : false;
 }
+/**
+ * Goes to a URL endpoint and puts data as JSON
+ * @param  string $url The URL to hit
+ * @param  mixed $data data to put in body as JSON
+ * @return mixed json value or false if failed
+ */
+function putUrlJson($url,$data){
+	print_r($url);
+	print_r(json_encode($data));
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data) );
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$data = curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	return ($httpcode>=200 && $httpcode<300) ? json_decode($data) : false;
+}
 
 /**
  * Goes to the student URL endpoint and grabs the data from it
@@ -28,33 +50,100 @@ function getStudent($student_id) {
 	return getUrlJson($url);
 }
 
+/**
+ * [putDataInElastic description]
+ *
+ * curl -XPUT "http://localhost:9200/movies/movie/1" -d'
+{
+    "title": "The Godfather",
+    "director": "Francis Ford Coppola",
+    "year": 1972,
+    "genres": ["Crime", "Drama"]
+}'
+ * @param  [type] $student_obj [description]
+ * @return [type]              [description]
+ */
+function putDataInElastic($student_id, $student_obj) {
+	$base_url = 'http://localhost:9200/raiseme/student/';
+	$url = $base_url . urlencode($student_id);
+	print_r(putUrlJson($url, $student_obj)); die;
+}
 
 $student_ids = getUrlJson('http://raise-me-take-home.raise.me');
+
+// This stores list of unique fields for student object (outside of courses)
 $student_fields = Array();
+// This stores list of unique fields for courses
 $course_fields = Array();
+
+// temporary counter to not spam the API
 $i = 0;
 
 foreach ($student_ids as $student_id) {
-	$student_data = getStudent($student_id);
+	$student_obj = getStudent($student_id);
 	// I can iterate over objects
-	foreach ($student_data as $key=>$value) {
-		if ( $key == 'courses' ) {
-			foreach ($value as $course_obj) {
+	foreach ($student_obj as $student_key=>$student_value) {
+		if ( $student_key == 'courses' ) {
+			$grade_total = 0.0;
+			$grade_num = 0;
+			foreach ($student_value as $course_obj) {
 				foreach ($course_obj as $course_key=>$course_value) {
 					if( !in_array($course_key, $course_fields) ){
         				$course_fields[] = $course_key;
 			        }
 				}
+				if ($course_obj->gradeValue) {
+					$grade_total += $course_obj->gradeValue;
+					++$grade_num;
+				} elseif ($course_obj->grade) {
+					// TODO: this should be more robust in cases there is A+ B-, etc.
+					switch ($course_obj->grade) {
+						case 'A':
+							$grade_total += 4.0;
+							++$grade_num;
+							break;
+						case 'B':
+							$grade_total += 3.0;
+							++$grade_num;
+							break;
+						case 'C':
+							$grade_total += 2.0;
+							++$grade_num;
+							break;
+						case 'D':
+							$grade_total += 1.0;
+							++$grade_num;
+							break;
+						case 'F':
+							$grade_total += 0;
+							++$grade_num;
+							break;
+					}
+				} // Else course is incomplete
 			}
+			// save grade to object
+			if ( $grade_num > 0 ) {
+				$student_obj->GPA = $grade_total / $grade_num;
+			}
+			//var_dump($student_obj->GPA);
 			continue; // don't put the courses inside $student_fields !
 		}
-		if ( !in_array($key, $student_fields) ) {
-			$student_fields[] = $key;
+
+		// find student fields
+		if ( !in_array($student_key, $student_fields) ) {
+			$student_fields[] = $student_key;
 		}
+
+		//print_r($student_obj);
 	}
+
+	// INSERT DATA INTO ELASTICSEARCH
+	putDataInElastic($student_id, $student_obj);
+
 	++$i;
 	if ($i == 10) { break; }
 }
+
 echo "STUDENT FIELDS = ";
 print_r($student_fields);
 echo "COURSE FIELDS = ";
