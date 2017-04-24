7fdb27ea845ed52822727a42076c99795691d282
terry chay <tychay@php.net>
2017-04-24 10:38:54 -0700

    iterate through everything and grab all the fields
    Didn't ssee a difference between the first student and other students.
    
    Signed-off-by: terry chay <tychay@php.net>

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@
 /**
  * Goes to a URL endpoint and returns the HTTP body as a JSON object
  * @param  string $url The URL to hit
- * @return array|false
+ * @return mixed json value or false if failed
  */
 function getUrlJson($url){
 	$ch = curl_init();
@@ -18,6 +18,46 @@ function getUrlJson($url){
 	return ($httpcode>=200 && $httpcode<300) ? json_decode($data) : false;
 }
 
-print_r(getUrlJson('http://raise-me-take-home.raise.me'));
+/**
+ * Goes to the student URL endpoint and grabs the data from it
+ * @param  string $student_id Student Id
+ * @return Object|false
+ */
+function getStudent($student_id) {
+	$url = 'http://raise-me-take-home.raise.me/'.urlencode($student_id).'.json';
+	return getUrlJson($url);
+}
+
+
+$student_ids = getUrlJson('http://raise-me-take-home.raise.me');
+$student_fields = Array();
+$course_fields = Array();
+$i = 0;
+
+foreach ($student_ids as $student_id) {
+	$student_data = getStudent($student_id);
+	// I can iterate over objects
+	foreach ($student_data as $key=>$value) {
+		if ( $key == 'courses' ) {
+			foreach ($value as $course_obj) {
+				foreach ($course_obj as $course_key=>$course_value) {
+					if( !in_array($course_key, $course_fields) ){
+        				$course_fields[] = $course_key;
+			        }
+				}
+			}
+			continue; // don't put the courses inside $student_fields !
+		}
+		if ( !in_array($key, $student_fields) ) {
+			$student_fields[] = $key;
+		}
+	}
+	++$i;
+	if ($i == 10) { break; }
+}
+echo "STUDENT FIELDS = ";
+print_r($student_fields);
+echo "COURSE FIELDS = ";
+print_r($course_fields);
 
 ?>
